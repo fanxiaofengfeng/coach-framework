520fd45666d18c7f8b31b3d4c1941bd6d8083089
fanxiaofeng <fanxiaofeng@qianlima.com>
2023-07-27 17:01:27 +0800

    刷题小程序项目初始化

---
+++ src/main/java/com/amber/coach/CoachFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.amber.coach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoachFrameworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoachFrameworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/amber/coach/CoachFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amber.coach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoachFrameworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
